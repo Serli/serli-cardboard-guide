007c0642311ec6a9ea9372a60ee07e64c997fd4b
Itchae <lucille.moise@gmail.com>
2017-06-23 15:24:30 +0200

    IHM
    Modification des boutons pour qu'ils soient transparents

---
+++ Assets/Scripts/SetTransparancy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetTransparancy : MonoBehaviour {
+	public Button myButton;
+	// Use this for initialization
+	void Start () {
+		myButton.image.color = new Color(255f,255f,255f,.2f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
